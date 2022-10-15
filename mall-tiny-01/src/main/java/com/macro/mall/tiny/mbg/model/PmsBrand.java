8dab8c5197d8c040d379e94a7b100775ce5a371e
yeqb <477613101@qq.com>
2022-10-15 23:00:23 +0800

    使用lombok；实现jrbel热部署

--- mall-tiny-01/src/main/java/com/macro/mall/tiny/mbg/model/PmsBrand.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/mbg/model/PmsBrand.java
@@ -1,7 +1,10 @@
 package com.macro.mall.tiny.mbg.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class PmsBrand implements Serializable {
     private Long id;
 
@@ -61,94 +64,6 @@ public class PmsBrand implements Serializable {
     private String brandStory;
 
     private static final long serialVersionUID = 1L;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getFirstLetter() {
-        return firstLetter;
-    }
-
-    public void setFirstLetter(String firstLetter) {
-        this.firstLetter = firstLetter;
-    }
-
-    public Integer getSort() {
-        return sort;
-    }
-
-    public void setSort(Integer sort) {
-        this.sort = sort;
-    }
-
-    public Integer getFactoryStatus() {
-        return factoryStatus;
-    }
-
-    public void setFactoryStatus(Integer factoryStatus) {
-        this.factoryStatus = factoryStatus;
-    }
-
-    public Integer getShowStatus() {
-        return showStatus;
-    }
-
-    public void setShowStatus(Integer showStatus) {
-        this.showStatus = showStatus;
-    }
-
-    public Integer getProductCount() {
-        return productCount;
-    }
-
-    public void setProductCount(Integer productCount) {
-        this.productCount = productCount;
-    }
-
-    public Integer getProductCommentCount() {
-        return productCommentCount;
-    }
-
-    public void setProductCommentCount(Integer productCommentCount) {
-        this.productCommentCount = productCommentCount;
-    }
-
-    public String getLogo() {
-        return logo;
-    }
-
-    public void setLogo(String logo) {
-        this.logo = logo;
-    }
-
-    public String getBigPic() {
-        return bigPic;
-    }
-
-    public void setBigPic(String bigPic) {
-        this.bigPic = bigPic;
-    }
-
-    public String getBrandStory() {
-        return brandStory;
-    }
-
-    public void setBrandStory(String brandStory) {
-        this.brandStory = brandStory;
-    }
 
     @Override
     public String toString() {
